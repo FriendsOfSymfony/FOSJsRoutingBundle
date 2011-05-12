805c34da665a2428f978c17e9c0f5673d3a74ccc
William DURAND <william.durand1@gmail.com>
2011-05-12 20:56:43 +0200

    Added default files

---
+++ BazingaExposeRoutingBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bazinga\ExposeRoutingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * BazingaExposeRoutingBundle class.
+ *
+ * @author William DURAND <william.durand1@gmail.com>
+ */
+class BazingaExposeRoutingBundle extends Bundle
+{
+}
