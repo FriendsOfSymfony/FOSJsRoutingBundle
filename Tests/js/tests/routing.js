75c83c19e29eeb884da5dd2b1f9fe92edfe30c07
William DURAND <william.durand1@gmail.com>
2011-05-13 16:42:57 +0200

    Added more js tests

--- Tests/js/tests/routing.js
+++ Tests/js/tests/routing.js
@@ -1,15 +1,16 @@
 test('api definition', function() {
-  expect(5);
+  expect(6);
 
   ok(Routing, 'Routing is defined');
   ok($.isFunction(Routing.connect), 'Routing.connect is a function');
   ok($.isFunction(Routing.generate), 'Routing.generate is a function');
   ok($.isFunction(Routing.get), 'Routing.get is a function');
   ok($.isFunction(Routing.has), 'Routing.has is a function');
+  ok($.isFunction(Routing.flush), 'Routing.flush is a function');
 });
 
 test('route registration', function() {
-  expect(9);
+  expect(11);
 
   strictEqual(Routing.connect('route_1', '/route1'), Routing,
               'connect "route_1". Routing.connect returns Routing');
@@ -23,74 +24,80 @@ test('route registration', function() {
   ok(Routing.has('route_2'), 'route_2 is connected');
   equal(Routing.get('route_1'), '/route1', 'route_1 url is still correct');
   equal(Routing.get('route_2'), '/route2', 'route_2 url is correct');
+
+  Routing.flush();
+  strictEqual(Routing.connect('route_1', '/route1'), Routing,
+              'connect "route_1". Routing.connect returns Routing');
+  ok(Routing.has('route_1'), 'route_1 is connected');
 });
 
 test('route generation', function() {
   expect(14);
 
   equal(Routing.connect('route_1', '/route1').generate('route_1'), '/route1',
-                              'generating url without parameters returns url');
+      'generating url without parameters returns url');
 
   equal(Routing.generate('route_1', { foo: 'bar' }), '/route1?foo=bar',
-                        'passing extra parameters happens it as query string');
+      'passing extra parameters happens it as query string');
 
   equal(Routing.connect('route_1', '/route1?a=b')
                               .generate('route_1', { foo: 'bar' }),
-                              '/route1?a=b&foo=bar',
-                              'query string is extended if already started');
+      '/route1?a=b&foo=bar',
+      'query string is extended if already started');
 
   equal(Routing.connect('route_1', '/route/{id}/edit')
                               .generate('route_1', { id: 'bar' }),
-                              '/route/bar/edit',
-                              'basic parameter replacement is ok');
+      '/route/bar/edit',
+      'basic parameter replacement is ok');
 
   equal(Routing.connect('route_1', '/route/{id}')
                               .generate('route_1', { id: 'bar' }),
-                              '/route/bar',
-                              'end of string parameter replacement is ok');
+      '/route/bar',
+      'end of string parameter replacement is ok');
 
   equal(Routing.generate('route_1', { id: 'foo', foo: 'bar' }),
-                              '/route/foo?foo=bar',
-                        'passing extra parameters happens it as query string');
+      '/route/foo?foo=bar',
+      'passing extra parameters happens it as query string');
 
-  equal(Routing.connect('route_1', '/route/{identical}/id/{id}/id/{identical}/foo')
-                              .generate('route_1', { id: 'bar' }),
-                              '/route/{identical}/id/bar/id/{identical}/foo',
-                              'only exact variable matches.');
+  equal(Routing.connect('route_1',
+      '/route/{identical}/id/{id}/id/{identical}/foo'
+      ).generate('route_1', { id: 'bar' }),
+      '/route/{identical}/id/bar/id/{identical}/foo',
+      'only exact variable matches.');
 
   equal(Routing.connect('route_1', '{id}')
                               .generate('route_1', { id: 'bar' }),
-                              '/bar',
-                              'replacement without separator is ok');
+      '/bar',
+      'replacement without separator is ok');
 
   // check for prefix
   Routing.prefix = '/baz/';
   equal(Routing.connect('route_1', '{id}')
                               .generate('route_1', { id: 'bar' }),
-                              '/baz/bar',
-                              'prefix is added');
+      '/baz/bar',
+      'prefix is added');
   Routing.prefix = 'baz';
   equal(Routing.connect('route_1', '{id}')
                               .generate('route_1', { id: 'bar' }),
-                              '/baz/bar',
-                              'prefix is surrounded by slashes');
+      '/baz/bar',
+      'prefix is surrounded by slashes');
 
   // check for default parameters
   Routing.prefix = '';
   equal(Routing.connect('route_1', '/foo/{id}', { id: 120 })
                               .generate('route_1'),
-                              '/foo/120',
-                              'default parameter is using if no parameter is specified');
+      '/foo/120',
+      'default parameter is using if no parameter is specified');
   equal(Routing.connect('route_1', '/foo/{id}', { id: 120 })
                               .generate('route_1', { id: 210 }),
-                              '/foo/210',
-                              'default parameter is overrided if a valid parameter is specified');
+      '/foo/210',
+      'default parameter is overrided if a valid parameter is specified');
   equal(Routing.connect('route_1', '/foo/{id}', { id: 120 })
                               .generate('route_1', { di: 210 }),
-                              '/foo/120?di=210',
-                              'default parameter is using if a wrong parameter is specified');
+      '/foo/120?di=210',
+      'default parameter is using if a wrong parameter is specified');
   equal(Routing.connect('route_1', '/foo/{id}/val/{val}', { val: 10 })
                               .generate('route_1', { id: 120, val: 210 }),
-                              '/foo/120/val/210',
-                              'Default parameters work with multiple variables');
+      '/foo/120/val/210',
+      'Default parameters work with multiple variables');
 });
