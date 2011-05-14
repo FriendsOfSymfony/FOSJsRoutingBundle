0ae1e1841fbdedd8ef7ff06a0436a469b54c7260
William DURAND <william.durand1@gmail.com>
2011-05-14 23:37:05 +0200

    Fixed tests configuration

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,17 @@
 <?php
 
-require __DIR__.'/../../../../../app/autoload.php';
-require __DIR__.'/../../../../../app/AppKernel.php';
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
+$loader->register();
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Bazinga\\ExposeRoutingBundle\\')) {
+        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        require_once __DIR__.'/../'.$path;
+        return true;
+    }
+});
