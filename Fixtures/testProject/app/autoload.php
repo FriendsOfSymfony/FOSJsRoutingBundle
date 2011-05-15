6ed7481a9b8ced6c0af6b9336326e30c8e537520
William DURAND <william.durand1@gmail.com>
2011-05-15 02:38:00 +0200

    Added fixtures to test the bundle without any dependencies

---
+++ Fixtures/testProject/app/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Symfony'     => __DIR__.'/../vendor/symfony/src',
+));
+$loader->registerPrefixes(array(
+    'Twig_Extensions_'   => __DIR__.'/../vendor/twig-extensions/lib',
+    'Twig_'              => __DIR__.'/../vendor/twig/lib',
+));
+$loader->register();
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Bazinga\ExposeRoutingBundle\\')) {
+        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        require_once __DIR__.'/../../../'.$path;
+        return true;
+    }
+});
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,17 +1,9 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../Fixtures/testProject/app/autoload.php';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'Bazinga\\ExposeRoutingBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
+$filesystem = new \Symfony\Component\HttpKernel\Util\Filesystem();
+$filesystem->remove(__DIR__.'/../Fixtures/testProject/app/cache');
+$filesystem->remove(__DIR__.'/../Fixtures/testProject/app/log');
+$filesystem->mkdir(__DIR__.'/../Fixtures/testProject/app/cache');
+$filesystem->mkdir(__DIR__.'/../Fixtures/testProject/app/log');
