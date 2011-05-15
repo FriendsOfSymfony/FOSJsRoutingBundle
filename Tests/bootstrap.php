5bcac5d6e1ce400cc99047bd7a5a28f3c19ea822
William DURAND <william.durand1@gmail.com>
2011-05-15 13:54:33 +0200

    Fixed bootstrap: log -> logs

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,6 +4,6 @@ require_once __DIR__.'/../Fixtures/testProject/app/autoload.php';
 
 $filesystem = new \Symfony\Component\HttpKernel\Util\Filesystem();
 $filesystem->remove(__DIR__.'/../Fixtures/testProject/app/cache');
-$filesystem->remove(__DIR__.'/../Fixtures/testProject/app/log');
+$filesystem->remove(__DIR__.'/../Fixtures/testProject/app/logs');
 $filesystem->mkdir(__DIR__.'/../Fixtures/testProject/app/cache');
-$filesystem->mkdir(__DIR__.'/../Fixtures/testProject/app/log');
+$filesystem->mkdir(__DIR__.'/../Fixtures/testProject/app/logs');
