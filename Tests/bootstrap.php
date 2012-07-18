15fb55255a60a3d1907b681931da600016bd634c
Yaroslav Kiliba <om.dattaya@gmail.com>
2012-07-18 17:36:03 +0300

    Updated .travis.yml, cleaned Tests/bootstrap, removed vendors.php.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,17 +9,6 @@
  * file that was distributed with this source code.
  */
 
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'FOS\\JsRoutingBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
-
 if (file_exists($loader = __DIR__.'/../vendor/autoload.php')) {
     require_once $loader;
 }
